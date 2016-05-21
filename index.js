6bf4b49d3a0812054884579a929770cb37195c05
xiaomo <83387856@qq.com>
2016-05-21 15:48:46 +0800

    eventutil

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+    * 把今天最好的表现当作明天最新的起点．．～
+    * いま 最高の表現 として 明日最新の始発．．～
+    * Today the best performance  as tomorrow newest starter!
+    * Created by IntelliJ IDEA.
+    *
+    * @author: xiaomo
+    * @github: https://github.com/qq83387856
+    * @email: hupengbest@163.com
+    * @QQ_NO: 83387856
+    * @Date: 2016/5/21 15:20
+    * @Description:
+    * @Copyright(©) 2015 by xiaomo.
+    **/
+require('./lib/EventUtil.js');
