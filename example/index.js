0543b9e619ed37fd6cff8975a9a1a44d64f81ec7
xiaomo <83387856@qq.com>
2016-05-21 17:01:39 +0800

    修改一下测试

--- example/index.js
+++ example/index.js
@@ -1,18 +1,4 @@
 /**
-    * 把今天最好的表现当作明天最新的起点．．～
-    * いま 最高の表現 として 明日最新の始発．．～
-    * Today the best performance  as tomorrow newest starter!
-    * Created by IntelliJ IDEA.
-    *
-    * @author: xiaomo
-    * @github: https://github.com/qq83387856
-    * @email: hupengbest@163.com
-    * @QQ_NO: 83387856
-    * @Date: 2016/5/21 16:05
-    * @Description:
-    * @Copyright(©) 2015 by xiaomo.
-    **/
-/**
  * 把今天最好的表现当作明天最新的起点．．～
  * いま 最高の表現 として 明日最新の始発．．～
  * Today the best performance  as tomorrow newest starter!
@@ -22,14 +8,13 @@
  * @github: https://github.com/qq83387856
  * @email: hupengbest@163.com
  * @QQ_NO: 83387856
- * @Date: 2016/5/21 16:04
+ * @Date: 2016/5/21 16:05
  * @Description:
  * @Copyright(©) 2015 by xiaomo.
  **/
-
 import EventUtil from 'EventUtil';
-
-var button = document.getElementById('btn');
-EventUtil.addHandler(button,'click',function () {
-    console.log('测试EventUtil')
-});
+var btn = document.getElementById('btn');
+var handler = function (event) {
+   alert("测试EventUtil");
+};
+EventUtil.addHandler(btn, "click", handler);
